00ec2bf879b087cfdea08c961ab5d9b1521c62a7
Takaho Takemura <jp.takapon@gmail.com>
2024-08-29 18:42:10 +0900

    usernameを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks",
+    passwords: "users/passwords",
+    sessions: "users/sessions",
+    unlocks: "users/unlocks"
+  }
   root "static_pages#home"
   get "/home", to: "static_pages#home"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_28_145630) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_29_074235) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "username", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -21,5 +22,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_28_145630) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
